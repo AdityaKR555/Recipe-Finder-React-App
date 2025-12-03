5a0666d60e3c950a1326d475dd12f29981290ff8
Aditya Kumar <adityaforwork101@gmail.com>
2025-12-03 19:58:20 +0530

    replaced link with navLink in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
@@ -9,10 +9,22 @@ const Navbar = () => {
               {/* <img src="../../public/recipefinderlogo.png" className='w-full h-full object-cover' alt="logo" /> */}
               <img src="/recipefinderlogo.png" className='w-full h-full object-cover' alt="logo" />
             </div>
-            <div className='flex items-center md:gap-16 gap-5 md:text-xl text-sm text-white'>
-              <Link to="/" className='hover:text-yellow-500 hover:font-semibold'>Home</Link>
-              <Link to="/categories" className='hover:text-yellow-500 hover:font-semibold'>Categories</Link>
-              <Link to="/fav" className='hover:text-yellow-500 hover:font-semibold'>Favourites</Link>
+            <div className='flex items-center md:gap-16 gap-5 md:text-xl text-sm'>
+              <NavLink to="/" className='hover:text-yellow-500 hover:font-semibold' 
+              style={({ isActive }) => ({
+                    color: isActive ? "orange" : "white",
+                    textDecoration: "none",
+                  })}>Home</NavLink>
+              <NavLink to="/categories" className='hover:text-yellow-500 hover:font-semibold' 
+              style={({ isActive }) => ({
+                    color: isActive ? "orange" : "white",
+                    textDecoration: "none",
+                  })}>Categories</NavLink>
+              <NavLink to="/fav" className='hover:text-yellow-500 hover:font-semibold' 
+              style={({ isActive }) => ({
+                    color: isActive ? "orange" : "white",
+                    textDecoration: "none",
+                  })}>Favourites</NavLink>
             </div>
         </div>
     </div>
